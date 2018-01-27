92880c8667be8ef9b51bc7b4925da73a1fc93cd0
Antoine <zertaps@gmail.com>
2018-01-27 17:35:59 +0100

    Contract : isOwner

--- contract/BlogChain.sol
+++ contract/BlogChain.sol
@@ -2,40 +2,51 @@ pragma solidity ^0.4.13;
 
 contract Blogchain {
     
+    address private owner;
+    
     struct Post {
         string content;
         string title;
     }
     
-    Post[] private posts;
-    
-    function Blogchain() public {
-        addPost("Caraibes", "plage et tout");
+    modifier isOwner {
+        require(owner == msg.sender);
+        _;
     }
     
-    function addPost(string title, string content) public 
-    returns(bool) {
-        
+    Post[] private posts;
+    
+    function Blogchain(address o) public {
+        posts.push(Post("Caraibes", "plage et tout"));
+        owner = o;
+    }
+    
+    function addPost(string title, string content) public isOwner 
+    returns(bool) 
+    {    
         posts.push(Post(title, content));
         return true;
     }
     
     function getPost(uint index) public constant
-    returns(string, string) {
-        
+    returns(string, string) 
+    {
         Post memory post = posts[index];
         return (post.title, post.content);
     }
     
-    function editPost(uint index, string title, string content) public
-    returns(bool) {
-        
+    function editPost(uint index, string title, string content) public isOwner
+    returns(bool) 
+    {
         posts[index] = Post(title, content);
         return true;
     }
     
-    function removePost(uint index) public returns(bool) {
-        if (index >= posts.length) return;
+    function removePost(uint index) public isOwner 
+    returns(bool) 
+    {   
+        if (index >= posts.length) 
+            return;
 
         for (uint i = index; i<posts.length-1; i++){
             posts[i] = posts[i+1];
@@ -45,7 +56,10 @@ contract Blogchain {
         return true;
     }
     
-    function getNbPosts() public constant returns (uint) {
+    function getNbPosts() public constant 
+    returns (uint) 
+    {
         return posts.length;
     }
+    
 }
